ab9b7a45db5d8ef43c52b10ae865b33facedb590
walterfish <funcat50@gmail.com>
2026-06-18 02:33:37 -0400

    saving for the night

--- GUIversion/graphicaltest.py
+++ GUIversion/graphicaltest.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from rock import Rock
+
 class scissorrock:
 	"""managing game assets sha-"""
 
@@ -11,12 +13,15 @@ class scissorrock:
 
 		pygame.display.set_caption("rock paper dodechahedreaon scissors")
 
+		self.screen = pygame.display.set_mode((1200, 800))
+
+		self.rock = Rock(self)
+
 	def run_game(self):
 		"""starts the loop for the game, primary"""
-		window = pygame.display.set_mode((1200, 800))
 		clock = pygame.time.Clock()
 
-		background = pygame.Surface(window.get_size())
+		background = pygame.Surface(self.screen.get_size())
 		ts, w, h, c1, c2 = 50, *background.get_size(), (128, 128, 128), (64, 64, 64)
 		tiles = [((x*ts, y*ts, ts, ts), c1 if (x+y) % 2 == 0 else c2) for x in range((w+ts-1)//ts) for y in range((h+ts-1)//ts)]
 		[pygame.draw.rect(background, color, rect) for rect, color in tiles]
@@ -26,10 +31,13 @@ class scissorrock:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					sys.quit()
+
 			# checkerboard background
-			window.blit(background, (0, 0))
+			self.screen.blit(background, (0, 0))
 			pygame.display.flip()
 			clock.tick(60)
+			self.rock.blitme()
+
 			# makes last screen visable
 			pygame.display.flip()
 
