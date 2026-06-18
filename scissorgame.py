1de0aad85066338f776bd7bdee277080d24a45c9
walterfish <funcat50@gmail.com>
2026-06-18 00:21:18 -0400

    changed some code for it to have shortcuts and a quit now

--- scissorgame.py
+++ scissorgame.py
@@ -7,47 +7,51 @@ aiplayer = 0
 # actual gameplay
 while True:
 	# sets up the game for the end user and shit lmao, i WILL forget how this is made
-	playerchoice = input("Rock, paper, dodecahedreon, or sizzor")
-	choices = ["rock", "paper", "dodecahedreon", "sizzor"]
+	playerchoice = input("rock (r), paper (p), dodecahedreon (d), or scizzors (s) (press q to quit) ")
+	choices = ["r", "p", "d", "s"]
 	aichoice = random.choice(choices)
 	# prints result for person to see
-	print("yo choice is " + choice + ". the robot chose " + aichoice + ".")
+	print("yo choice is " + playerchoice + ". the robot chose " + aichoice + ".")
 
 	# logic for scoring
 	if playerchoice == aichoice: 
 		print("Tie!")
-	elif playerchoice == "paper":
-		if aiplayer == "rock":
+	elif playerchoice == "p":
+		if aichoice == "r":
 			print("victory")
 			player += 1
 		else:
-			print("epic stewie fail, make the computer choose rock next time fool ;p")
+			print("epic stewie fail, make the computer choose r next time fool ;p")
 			aiplayer += 1
-	elif playerchoice == "rock":
-		if aiplayer == "sizzor":
+	elif playerchoice == "r":
+		if aichoice == "s":
 			print("good job!")
 			player += 1
 		else:
 			print("womp womp womp wompppp~...")
 			aiplayer += 1
-	elif playerchoice == "sizzor":
-		if aiplayer == "paper":
+	elif playerchoice == "s":
+		if aichoice == "p":
 			print("ayyeeeee nice!")
 			player += 1
 		else:
 			print("just get good...")
 			aiplayer += 1
-	elif playerchoice == "dodecahedreon":
-		if aiplayer == "rock":
+	elif playerchoice == "d":
+		if aichoice == "r":
 			print("why are you so bad at this game? SUPER F!!!")
 			aiplayer += 20
-		elif aiplayer == "paper":
-			print("alas, you cannot make a dodecahedreon on a piece of paper, so YOU WIN!!!")
+		elif aichoice == "p":
+			print("alas, you cannot make a dodecahedreon on a piece of p, so YOU WIN!!!")
 			player += 20
-		else:
+		if aichoice == "s":
 			print("you cant cut a dodecahedreon you clanker")
 			player += 20
+	# in case they want to ragequit
+	elif playerchoice == "q":
+		break
 
 	#printing final scores
-	print("human: " + player + " points")
-	print("robot: " + aiplayer + " points")
+	print("human: %s" % player)
+	print("robot: %s" % aiplayer)
+	print("																				")
