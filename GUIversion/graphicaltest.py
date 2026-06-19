b8290bf17c4d620c8260fa7307b27351ecd23437
walterfish <funcat50@gmail.com>
2026-06-19 13:38:57 -0400

    edited graphicaltest.py

--- GUIversion/graphicaltest.py
+++ GUIversion/graphicaltest.py
@@ -20,6 +20,13 @@ class scissorrock:
 		pygame.init()
 
 		self.can_clik = True # toggle
+		self.choice = False # choice module
+		self.score_calculated = False
+		self.aiplayer = 0
+		self.player = 0
+
+		self.text_x = 100
+		self.text_y = 150
 
 		pygame.display.set_caption("rock paper dodechahedreaon scissors")
 
@@ -39,6 +46,7 @@ class scissorrock:
 	def run_game(self):
 		"""starts the loop for the game, primary"""
 		clock = pygame.time.Clock()
+		font = pygame.font.SysFont(None, 48)
 
 		background = pygame.Surface(self.screen.get_size())
 		ts, w, h, c1, c2 = 50, *background.get_size(), (128, 128, 128), (64, 64, 64)
@@ -55,13 +63,14 @@ class scissorrock:
 
 				if event.type == pygame.MOUSEBUTTONUP:
 					self.can_clik = True
+					self.choice = False
 
 			# checkerboard background
 			self.screen.blit(background, (0, 0))
 			clock.tick(60)
 
 			# make the clanker choose
-			choices = ["r", "p", "d", "s"]
+			choices = ["rock", "paper", "objectd", "scissors"]
 			aichoice = random.choice(choices)
 
 			#asset loading
@@ -69,25 +78,58 @@ class scissorrock:
 				rock.blitme()
 				if pygame.mouse.get_pressed()[0] and rock.rect.collidepoint(mouse_pos) and self.can_clik:
 					self.can_clik = False
+					self.choice = "rock"
 					print("it works for the rock")
 
 			for paper in self.paper:
 				paper.blitme()
 				if pygame.mouse.get_pressed()[0] and paper.rect.collidepoint(mouse_pos) and self.can_clik:
 					self.can_clik = False
-					print("it works for paper")
+					self.choice = "paper"
+					print(f"it works for paper and the computer chooses {aichoice}")
+					if aichoice == "rock":
+						print("victory")
+						self.player += 1
+					else:
+						print("epic stewie fail, make the computer choose r next time fool ;p")
+						self.aiplayer += 1
 			          
 			for scissor in self.scissor:
 				scissor.blitme()
 				if pygame.mouse.get_pressed()[0] and scissor.rect.collidepoint(mouse_pos) and self.can_clik:
 					self.can_clik = False
-					print("it works for the scissor")
+					self.choice = "scissors"
+					print("it works for " + self.choice)
+					if aichoice == "paper":
+						print("ayyeeeee nice!")
+						self.player += 1
+					else:
+						print("just get good...")
 
 			for objectd in self.objectd:
 				objectd.blitme()
 				if pygame.mouse.get_pressed()[0] and objectd.rect.collidepoint(mouse_pos) and self.can_clik:
 					self.can_clik = False
+					self.choice = "objectd"
 					print("it works for the objectd")
+					if aichoice == "r":
+						print("why are you so bad at this game? SUPER F!!!")
+						self.aiplayer += 20
+					elif aichoice == "p":
+						print("alas, you cannot make a dodecahedreon on a piece of p, so YOU WIN!!!")
+						self.player += 20
+					if aichoice == "s":
+						print("you cant cut a dodecahedreon you clanker")
+						self.player += 20
+
+			if self.choice and not self.score_calculated:
+				self.score_calculated = True
+				print(f"computa chose {aichoice}")
+				print("																				")
+
+			ubuntu_bold = pygame.font.Font('/usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf', 30)
+			score_text = pygame.font.Font.render(ubuntu_bold, 'player: {self.player} | computer: {self.aiplayer}', True, (255, 255, 255))
+			self.screen.blit(score_text, (250, 300))
 
 			# makes last screen visable
 			pygame.display.flip()
